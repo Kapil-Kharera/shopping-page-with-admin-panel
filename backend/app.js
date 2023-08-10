2c0323e3431dee51edcab01dfa21dd60aa92cdfa
Kapil Kharera <kapil95881@gmail.com>
2023-08-10 10:52:43 +0530

    Added sessions for user

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const expressSession = require("express-session");
+const csrf = require("csurf");
+
 const path = require("path");
 const db = require("./data/database");
-const csrf = require("csurf");
 const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
 const errorHandlerMiddleware = require("./middlewares/error-handler");
+const createSessionConfig = require("./config/session");
 
 const authRoutes = require("./routes/auth.routes");
 
@@ -15,6 +18,11 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: false }));
+
+const sessionConfig = createSessionConfig();
+
+//before csrf
+app.use(expressSession(sessionConfig));
 
 //before routes
 app.use(csrf());
