c8b8474f13ac9caff8f0f4cef7f68f6feeaa1fa5
Kapil Kharera <kapil95881@gmail.com>
2023-09-15 17:12:39 +0530

    Updated gitignore file

--- data/database.js
+++ data/database.js
@@ -2,13 +2,14 @@ const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
-let database;
+// let database;
 
 // let mongodbURI = "mongodb://localhost:27017";
 
 // if(process.env.MONGODB_URL) {
 //     mongodbURI = process.env.MONGODB_URL;
 // }
+
 
 async function connectToDatabase() {
    const client = await MongoClient.connect(process.env.MONGODB_URL);
