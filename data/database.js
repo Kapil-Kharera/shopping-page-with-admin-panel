0bd8a20d46bd1fbbbb806b46b1998c18538a8a1d
Kapil Kharera <kapil95881@gmail.com>
2023-09-19 10:38:14 +0530

    updated path for db again

--- data/database.js
+++ data/database.js
@@ -2,17 +2,17 @@ const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
-// let database;
+let database;
 
-// let mongodbURI = "mongodb://localhost:27017";
+let mongodbURI = "mongodb://localhost:27017";
 
-// if(process.env.MONGODB_URL) {
-//     mongodbURI = process.env.MONGODB_URL;
-// }
+if(process.env.MONGODB_URL) {
+    mongodbURI = process.env.MONGODB_URL;
+}
 
 
 async function connectToDatabase() {
-   const client = await MongoClient.connect(process.env.MONGODB_URL);
+   const client = await MongoClient.connect(mongodbURI);
    database = client.db("complete-online-shop");
 }
 
